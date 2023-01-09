c1917404c9c343e8bf376a8a03296699b65ffe2f
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-01-09 20:14:57 +0200

    add tests for Product model that involve deleting

--- test/Product.model.test.ts
+++ test/Product.model.test.ts
@@ -4,13 +4,14 @@ import { describe, it } from "mocha";
 import mongoose from "mongoose";
 import { start } from "../init/app.js";
 import Product from "../src/models/Product.model.js";
+import User from "../src/models/User.model.js";
 
 describe('Product model', () => {
     let testDB = 'mongodb://127.0.0.1/ecommerce-test';
 
     beforeEach(async () => {
         await start(express(), testDB);
-        
+
     });
 
     describe('Successful cases', async () => {
@@ -51,7 +52,50 @@ describe('Product model', () => {
             await product.save();
 
             expect(product.price).to.equal(0.01);
-        })
+        });
+
+        it('Is removed from users\'s product collections when deleted', async () => {
+            // Seed the test
+            const user1 = new User({ username: 'abcde', password: '123456' });
+            const user2 = new User({ username: 'abcde2', password: '123456' });
+
+            const owner = new User({ username: 'owner', password: '123456' });
+
+            await user1.save();
+            await user2.save();
+            await owner.save()
+
+            const product = new Product(
+                { 
+                    name: 'abcde', 
+                    price: 0.01, 
+                    image: 'a', 
+                    buyers: [user1, user2],
+                    owner
+                }
+            );
+            await product.save();
+
+            user1.boughtProducts.push(product);
+            user2.boughtProducts.push(product);
+
+            await user1.save();
+            await user2.save();
+
+            // Delete the product and compare
+            await Product.findOneAndDelete({ 
+                _id: product._id,
+            });
+            
+            const buyer1 = await User.findById(user1._id);
+            const buyer2 = await User.findById(user2._id);
+            const ownerOfDeletedProduct = await User.findById(owner._id);
+            expect(buyer1?.boughtProducts.length).to.equal(0);
+            expect(buyer2?.boughtProducts.length).to.equal(0);
+            expect(ownerOfDeletedProduct?.products.length).to.equal(0);
+            
+
+        });
     });
 
     describe('Name', async () => {
