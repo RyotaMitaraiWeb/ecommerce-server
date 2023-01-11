bb265dd59d4fdff56e5447b4b1b65ea553789c50
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-01-11 16:01:42 +0200

    expand the IRequest interface with product-related properties

--- util/IRequest.d.ts
+++ util/IRequest.d.ts
@@ -1,6 +1,10 @@
 import { Request } from "express";
 import { IUserState } from "../middlewares/session.middleware";
+import { IProduct } from "../src/models/Product.model";
 
 export interface IRequest extends Request {
     user: IUserState;
+    product: IProduct;
+    isOwner: boolean;
+    hasBought: boolean;
 }
