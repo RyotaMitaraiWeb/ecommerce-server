3840e7d5d16f6b54a5678834b1419f4f75f09707
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-01-12 23:54:43 +0200

    add HttpError.ts

---
+++ util/HttpError.ts
@@ -0,0 +1,20 @@
+/**
+ * This class can be used in place of the standard ``Error`` constructor to throw errors.
+ * It allows you to specify the status code of the error, allowing you to 
+ * handle different types of errors within the same request
+ * without resorting to complex error handling.
+ * **Note:** this class cannot be used with Mongoose's built-in validations. For controllers
+ * that may throw a validation error, you need to provide a "fallback" status code to the
+ * response object when handling a possible error
+ */
+export class HttpError extends Error {
+    constructor(message: string, status: number) {
+        super(message)
+
+        this.status = status;
+        this.name = 'HttpError';
+    }
+
+    status: number;
+    name: string;
+}
